867f96c912f00de7996a52e3f3d3d24532f361dd
Javier Mengual <javiermengual@live.com>
2025-07-05 14:57:47 +0200

    Actualiza .gitignore y remueve archivos compilados

--- src/main/java/persistance/PersistanceController.java
+++ src/main/java/persistance/PersistanceController.java
@@ -1,42 +1,28 @@
 package persistance;
 
-
 import persistance.entity.Movie;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PersistanceController {
-
-    private final Map<Integer, Movie> movies;
-    private int movieCount;
-
-    public PersistanceController() {
-        this.movies = new HashMap<>();
-        this.movieCount = 0;
-    }
+    private int movieCount = 0;
+    private String dbUrl = "jdbc:postgresql://localhost:5432/movies";
+    private String dbUser = "root";
+    private String dbPassword = "1234";
 
     public int getMovieCount() {
         return movieCount;
     }
 
+
     public void saveMovie(Movie movie) {
-        movies.put(movie.getId(), movie);
-        movieCount++;
-    }
+        try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
 
-    public List<Movie> getAllMovies() {
-        return movies.values().stream().toList(); // mutabilidad? new?
-    }
-
-    public Optional<Movie> getMovieById(int id) {
-        return Optional.ofNullable(movies.get(id));
-    }
-
-    public Optional<Movie> deleteMovieById(int id) {
-        return Optional.ofNullable(movies.remove(id));
+        } catch (SQLException e) {
+            System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
